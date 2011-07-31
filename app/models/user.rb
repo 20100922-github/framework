628e0615b1efd00e8bc4ab421a9f3df9a0d71641
20100922-github <20100922-github@schleinkofer.name>
2011-07-31 20:14:52 +0200

    new icon

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   has_and_belongs_to_many :roles
   has_many :folders
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
