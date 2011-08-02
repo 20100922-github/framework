fe789c220834efbac194f908cf1e0b002bd2b2b8
20100922-github <20100922-github@schleinkofer.name>
2011-08-02 07:19:48 +0200

    styling, tags, comments

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,12 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :media
+  after_save :delete
+
+  def delete
+    if (self.media.size == 0)
+      logger.debug "blub"
+    else
+      logger.debug "bla"
+    end
+  end
 end
