7330fbc08c075ea5c5120a0e729d0f096024ee15
20100922-github <20100922-github@schleinkofer.name>
2011-08-15 18:20:24 +0200

    layout

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,33 +6,11 @@ class Ability
     user ||= User.new
     if user.role? :admin
       can :manage, :all
-    else
+    elsif user.role? :registered
       can [:read, :download_medium, :download_quadratic], Folder, :roles => { :id => user.role_ids }
       can [:read, :download_original, :download_medium, :download_thumb, :download_quadratic], Medium, :folder => { :roles => { :id => user.role_ids }}
       can :read, Comment, :medium => { :folder => { :roles => { :id => user.role_ids }}}
-      #can [:read, :download_original, :download_medium, :download_thumb, :download_quadratic], [Folder, Tag, Medium]
+    else
     end
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user permission to do.
-    # If you pass :manage it will apply to every action. Other common actions here are
-    # :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. If you pass
-    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
   end
 end
