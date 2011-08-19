cbda39823c47aaa7e55b6e743834ff3831884d95
20100922-github <20100922-github@schleinkofer.name>
2011-08-19 14:47:26 +0200

    roles vs groups. roles not yet working @ users form

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,10 +6,10 @@ class Ability
     user ||= User.new
     if user.role? :Administrator
       can :manage, :all
-    elsif user.role? :registered
-      can [:read, :download_medium, :download_quadratic], Folder, :roles => { :id => user.role_ids }
-      can [:read, :download_original, :download_medium, :download_thumb, :download_quadratic], Medium, :folder => { :roles => { :id => user.role_ids }}
-      can :read, Comment, :medium => { :folder => { :roles => { :id => user.role_ids }}}
+    elsif user.role? :Users
+      can [:read, :download_medium, :download_quadratic], Folder, :groups => { :id => user.group_ids }
+      can [:read, :download_original, :download_medium, :download_thumb, :download_quadratic], Medium, :folder => { :groups => { :id => user.group_ids }}
+      can :read, Comment, :medium => { :folder => { :groups => { :id => user.group_ids }}}
     else
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,6 @@
 Role.create(:name => 'Administrator', :description => 'Users, that have control over every aspect of this framework', :created_at => '2000-01-01 00:00:00.000000', :updated_at => '2000-01-01 00:00:00.000000')
 Role.create(:name => 'Users', :description => 'Regular users, that can e.g. create folders, add media and comment on it', :created_at => '2000-01-01 00:00:00.000000', :updated_at => '2000-01-01 00:00:00.000000')
 User.create(:email => '20100922-github@schleinkofer.name', :password => 'administrator', :password_confirmation => 'administrator', :username => 'administrator', :roles => Role.where(:name => 'Administrator'))
+Group.create(:name => 'Family', :description => 'All family members', :created_at => '2000-01-01 00:00:00.000000', :updated_at => '2000-01-01 00:00:00.000000')
+Group.create(:name => 'Friends', :description => 'All friends', :created_at => '2000-01-01 00:00:00.000000', :updated_at => '2000-01-01 00:00:00.000000')
+Group.create(:name => 'Co-Workers', :description => 'All co-workers', :created_at => '2000-01-01 00:00:00.000000', :updated_at => '2000-01-01 00:00:00.000000')
