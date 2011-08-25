c63869f1464b552baf3f0b79286a5bd27ae82942
20100922-github <20100922-github@schleinkofer.name>
2011-08-25 21:12:55 +0200

    abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,19 +3,25 @@ class Ability
 
   def initialize(user)
 
+    alias_action :read, :download_thumb, :download_medium, :download_quadratic, :download_original, :to => :read_all
+
     #user ||= User.new
     unless user
-      can :read, Folder, :roles => { :name => "Public" }
-      can :read, Medium, :folder => { :roles => { :name => "Public" }}
+
+      can :read_all, Folder, :roles => { :name => "Public" }
+      can :read_all, Medium, :folder => { :roles => { :name => "Public" }}
       cannot :manage, [User, Role]
+
     else
-      can [:read, :download_medium, :download_quadratic], Folder, :roles => { :id => user.role_ids }
-      can [:read, :download_original, :download_medium, :download_thumb, :download_quadratic], Medium, :folder => { :roles => { :id => user.role_ids }}
+
+      can :read_all, Folder, :roles => { :id => user.role_ids }
+      can :read_all, Medium, :folder => { :roles => { :id => user.role_ids }}
       can :read, Comment, :medium => { :folder => { :roles => { :id => user.role_ids }}}
-      can :manage, User, :id => user.id
+      can :update, User, :id => user.id
       cannot :manage, [User, Role]
 
       if user.role? :Administrator
+        cannot [:update, :destroy], Role, :name => "Administrator"
         can :manage, :all
       end
     end
