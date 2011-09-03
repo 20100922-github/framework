54b952f8bbb89087463e9b18e5edd299a40064b1
20100922-github <20100922-github@schleinkofer.name>
2011-09-03 11:23:20 +0200

    layouting, seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-Role.create(:name => 'Administrator', :description => 'Users, that have control over every aspect of this framework', :created_at => '2000-01-01 00:00:00.000000', :updated_at => '2000-01-01 00:00:00.000000')
-Role.create(:name => 'Users', :description => 'Regular users, that can e.g. create folders, add media and comment on it', :created_at => '2000-01-01 00:00:00.000000', :updated_at => '2000-01-01 00:00:00.000000')
+Role.create(:name => 'Administrators', :description => 'Users, that have control over every aspect of this framework', :created_at => '2000-01-01 00:00:00.000000', :updated_at => '2000-01-01 00:00:00.000000')
 Role.create(:name => 'Public', :description => 'Every folder, that is marked as Public is readable world-wide without having to log-in', :created_at => '2000-01-01 00:00:00.000000', :updated_at => '2000-01-01 00:00:00.000000')
 User.create(:email => '20100922-github@schleinkofer.name', :password => 'administrator', :password_confirmation => 'administrator', :username => 'administrator', :roles => Role.where(:name => 'Administrator'))
